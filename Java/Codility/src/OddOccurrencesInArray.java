e7e65a02d26ce8da6fe4c5e9d1c045f1fbd5768e
Jenny <lilylee9292@gmail.com>
2019-03-30 20:35:19 +0900

    [Codility] OddOccurrencesInArray problem solving

---
+++ Java/Codility/src/OddOccurrencesInArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/**
+ * Q. 정수(홀수) N개의 수로 이루어진 배열 A의 동일한 값을 쌍으로 이루지 않은 요소를 찾아라.
+ * A. https://app.codility.com/demo/results/training8DZYSM-P7E/
+ * Test Score. 100%
+ */
+class Solution1 {
+    public int solution(int[] A) {
+        Map<Integer, Integer> aMap = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < A.length; i++) {
+            if (aMap.containsKey(A[i])) {
+                int value = aMap.get(A[i]) + 1;
+                aMap.put(A[i], value);
+            } else {
+                aMap.put(A[i], 1);
+            }
+        }
+
+        int aMapOddValue = 0;
+        for (Integer key : aMap.keySet()) {
+            aMapOddValue = (aMap.get(key) % 2 == 1) ? key : aMapOddValue;
+        }
+
+        return aMapOddValue;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        //solution();
+    }
+}
